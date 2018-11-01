49e7b3eefcf9373eacf6a4420015bcd6cdf28069
CertainLach <iam@lach.pw>
2018-11-01 21:51:05 +0500

    Handle 'count' field inside nested containers

--- index.js
+++ index.js
@@ -42,7 +42,10 @@ module.exports = (proto) => {
                         if (field.type instanceof Array) {
                             // Mapper and switch can't be extracted from container, because they refer to field in container
                             if (field.type[0] === 'mapper' || field.type[0] === 'switch') return;
+                            // Arrays which refers to count can't be extracted also
                             if (field.type[0] === 'array' && field.type[1].count) return;
+                            // Count field is attached to array
+                            if (field.type[0] === 'count') return;
                             let typeName = `extracted_${field.name || ('anon_' + (anonIdx++).toString(36))}_from_` + getPathToThisFromParentTypesContainer(this);
                             parent[typeName] = field.type;
                             field.type = typeName;
